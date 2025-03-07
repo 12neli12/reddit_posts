80e0dd5aa24b7b9144a15fd69f3362e98d275741
Mero <52600370+12neli12@users.noreply.github.com>
2025-03-07 11:07:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import prawcore
 import os
 from dotenv import load_dotenv
 
-def fetch_posts(name: str, post_num: int = 5):
+def fetch_posts(name, post_num= 5):
     try:
         # Load env variables from .env
         load_dotenv()
@@ -24,31 +24,22 @@ def fetch_posts(name: str, post_num: int = 5):
             print("Author: ", submission.author)
             print("Upvotes: ", submission.score)
 
-    # Network issues handling
+    # Error Handling
     except prawcore.exceptions.RequestException:
         print("Network error. Check your internet connection.")
 
-    # API rate-limiting handling
     except prawcore.exceptions.TooManyRequests:
-        print("Error. Rate-limited by Reddit.")
+        print("Error. Rate-limited.")
 
-    # Authentication issues handling
     except prawcore.exceptions.OAuthException:
-        print("Error. Check your ID, user agent, and secret.")
+        print("Error. Check ID, user agent, and secret.")
 
-    # Subreddit does not exist
     except prawcore.exceptions.NotFound:
         print(f"Error. Subreddit r/{name} not found.")
 
-    # Access-restricted handling
-    except prawcore.exceptions.Forbidden:
-        print("Error. Access forbidden.")
-
-    # PRAWCore general exception handling
     except prawcore.exceptions.PrawcoreException as e:
         print(f"Error: {e} ")
 
-    # Catch other errors
     except Exception as e:
         print("Unexpected error: ", e)
 
